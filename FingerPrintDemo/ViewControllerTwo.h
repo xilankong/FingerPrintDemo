e543fdaae0eff73f564f1dde5a9d828881be434c
young.huang <young.huang@jfz.com>
2016-11-29 16:22:43 +0800

    first commit

---
+++ FingerPrintDemo/ViewControllerTwo.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerTwo.h
+//  FingerPrintDemo
+//
+//  Created by yanghuang on 16/11/29.
+//  Copyright © 2016年 yanghuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerTwo : UIViewController
+
+@end
