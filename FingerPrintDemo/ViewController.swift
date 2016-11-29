70e048024c18a16f23c82665c1416e67f65cf246
young.huang <young.huang@jfz.com>
2016-11-29 16:24:39 +0800

    #mod

--- FingerPrintDemo/ViewController.swift
+++ FingerPrintDemo/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+    var btn : UIButton = {
+        var btn = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 100));
+        btn.setTitle("进入 OC验证", for: UIControlState.normal);
+        btn.setTitleColor(UIColor.blue, for: UIControlState.normal);
+        return btn;
+    }();
+    
+    var label : UILabel = {
+        var label = UILabel(frame: CGRect(x: 0, y: 150, width: 320, height: 40));
+        label.textColor = UIColor.black;
+        label.textAlignment = NSTextAlignment.center;
+        return label;
+    }();
 
     override func viewDidLoad() {
+        self.title = "Swift 指纹解锁";
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(btn);
+        self.view.addSubview(label);
+        btn.addTarget(self, action: #selector(goOC), for: UIControlEvents.touchUpInside)
+        
+        self.authenticate();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    func goOC() {
+        
+        let vc : UIViewController = ViewControllerTwo();
+        self.navigationController?.pushViewController(vc, animated: true);
+    }
+    
+    //指纹验证
+    func authenticate() {
+
+        if #available(iOS 8.0,OSX 10.12, *){ //IOS 版本判断 低于版本无需调用
+            let context : LAContext = LAContext();
+            var error : NSError? = nil;
+            let msg : String = "使用您设备解锁指纹解锁";
+            //判断设备支持状态
+            if context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                
+                //指纹验证
+                context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: msg, reply: { (success, errorTwo) in
+                    
+                    if success {
+                        //验证成功 直接更新UI慢的原因是因为没有切回主线程    需要切回主线程操作UI
+                        DispatchQueue.main.async {
+                            self.label.text = "成功解锁";
+                        }
+                    } else {
+                        //根据错误Code不一样，区分不同失败原因
+                        switch errorTwo! {
+                            
+                        case LAError.userCancel:
+                            DispatchQueue.main.async  {
+                                self.label.text = "用户取消验证";
+                            }
+                        break;
+                        case LAError.systemCancel:
+                            DispatchQueue.main.async  {
+                                self.label.text = "系统取消验证";
+                            }
+                            break;
+                        case LAError.userFallback:
+                            DispatchQueue.main.async  {
+                                self.label.text = "用户解锁失败";
+                            }
+                            break;
+                        default :
+                                DispatchQueue.main.async {
+                                    self.label.text = "其他原因解锁失败";
+                                }
+                            break;
+                            
+                        }
+                        
+                    }
+                    
+                })
+            } else {
+                DispatchQueue.main.async {
+                    self.label.text = "您的设备不支持指纹解锁";
+                }
+            }
+        
+        }
+        
+    }
 
 }
 
--- FingerPrintDemo/ViewControllerTwo.m
+++ FingerPrintDemo/ViewControllerTwo.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewControllerTwo.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface ViewControllerTwo ()
+
+@property (nonatomic, strong) UILabel *resultLabel;
 
 @end
 
@@ -16,22 +19,112 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.title = @"OC 指纹解锁";
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.resultLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 150, 320, 40)];
+    self.resultLabel.textColor = [UIColor blackColor];
+    self.resultLabel.textAlignment = NSTextAlignmentCenter;
+    self.resultLabel.text = @"";
+    [self.view addSubview:self.resultLabel];
+    
+    
+    [self authenticateUser];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+#pragma 指纹验证
+
+- (void)authenticateUser {
+    //初始化上下文
+    LAContext *context = [[LAContext alloc]init];
+    NSError *error = nil;
+    
+    NSString *msg = @"使用您设备解锁指纹解锁";
+    
+    //判断设备支持状态 
+    if([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+       //指纹验证
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:msg reply:^(BOOL success, NSError * _Nullable error) {
+            if (success) {
+                //验证成功 直接更新UI慢得原因是因为没有切回主线程    需要切回主线程操作UI
+                [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+                    
+                    self.resultLabel.text = @"成功解锁";
+                }];
+            } else {
+                NSLog(@"%@",error.localizedDescription);
+                switch (error.code) {
+                    case LAErrorSystemCancel:
+                        //切换到其他APP 系统取消验证
+                    {
+                        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+                            
+                            self.resultLabel.text = @"系统取消验证";
+                        }];
+                    }
+                        break;
+                    case LAErrorUserCancel:
+                        //用户取消验证
+                    {
+                        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+                            
+                            self.resultLabel.text = @"用户取消验证";
+                        }];
+                    }
+                        break;
+                    case LAErrorUserFallback:
+                        //用户验证失败
+                    {
+                        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+                            
+                            self.resultLabel.text = @"用户验证失败";
+                        }];
+                    }
+                        break;
+
+                    default:
+                        //其他失败情况
+                    {
+                        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+                            
+                            self.resultLabel.text = @"其他失败情况";
+                        }];
+                    }
+                        break;
+                }
+            }
+        }];
+        
+    } else {
+        
+    }
+    
+    
+}
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
+ //授权失败
+ LAErrorAuthenticationFailed = kLAErrorAuthenticationFailed,
+ //用户取消Touch ID授权
+ LAErrorUserCancel           = kLAErrorUserCancel,
+ 
+ //用户选择输入密码
+ LAErrorUserFallback         = kLAErrorUserFallback,
+ 
+ //系统取消授权(例如其他APP切入)
+ LAErrorSystemCancel         = kLAErrorSystemCancel,
+ 
+ //系统未设置密码
+ LAErrorPasscodeNotSet       = kLAErrorPasscodeNotSet,
+ 
+ //设备Touch ID不可用，例如未打开
+ 
+ LAErrorTouchIDNotAvailable  = kLAErrorTouchIDNotAvailable,
+ 
+ //设备Touch ID不可用，用户未录入
+ LAErrorTouchIDNotEnrolled   = kLAErrorTouchIDNotEnrolled,
+ */
 @end
